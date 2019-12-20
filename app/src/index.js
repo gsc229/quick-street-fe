87d477c83f88a1704f9e3d79d86abb9c111b96c6
Richard Lawson <lawson.a.richard@gmail.com>
2019-12-20 10:16:43 -0800

    made changes to index.js

--- app/src/index.js
+++ app/src/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-<<<<<<< HEAD
-import "./App.css";
-=======
 import { BrowserRouter as Router } from 'react-router-dom';
->>>>>>> master
 
 ReactDOM.render(
   <Router>

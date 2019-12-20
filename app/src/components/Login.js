cb832efeebf9e9c13e9677df2601e86f8a578e3f
Rashmi <poddar.rashmi@gmail.com>
2019-12-20 12:41:16 -0500

    Converted login component to functional component from class component

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,53 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import axiosWithAuth from '../utils/axiosWithAuth';
 
-class Login extends React.Component {
-  state = {
+const Login = () => {
+  const [ credentials, setCredentials ] = useState({
     email: '',
     password: ''
-  }
+  })
 
-  handleChange = (event) => {
-    this.setState({
-      ...this.state,
+  const handleChange = (event) => {
+    setCredentials({
+      ...credentials,
       [event.target.name] : event.target.value
     });
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state);
+    console.log(credentials);
     // implement post request once backend is deployed
   }
 
-  render() {
-    return(
-      <div>
-        <form onSubmit={this.handleSubmit}>
-        <label htmlFor='email'>Email</label>
-          <input
-            type='text'
-            name='email'
-            id='email'
-            placeholder='Enter your email'
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-          <label htmlFor='password'>Password</label>
-          <input 
-            type='password'
-            name='password'
-            id='password'
-            placeholder='Please enter a password'
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <button>Login</button>
-        </form>
-      </div>
-    )
-  }
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+      <label htmlFor='email'>Email</label>
+        <input
+          type='text'
+          name='email'
+          id='email'
+          placeholder='Enter your email'
+          value={credentials.email}
+          onChange={handleChange}
+        />
+        <label htmlFor='password'>Password</label>
+        <input 
+          type='password'
+          name='password'
+          id='password'
+          placeholder='Please enter a password'
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  )
+  
 }
 
 export default Login;

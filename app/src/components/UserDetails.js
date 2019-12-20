b8f51e202fc14a3eecdbf8f66819fee598156612
Rashmi <poddar.rashmi@gmail.com>
2019-12-20 13:22:32 -0500

    Added divs for styling

--- app/src/components/UserDetails.js
+++ app/src/components/UserDetails.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import meditateGirlImg from '../assets/meditate_girl_1.jpg';
+
 const UserDetails = (props) => {
   const proceed = (event) => {
     event.preventDefault();
@@ -9,49 +11,62 @@ const UserDetails = (props) => {
   const { values, handleChange} = props;
 
   return (
-    <div>
-    <h1>Create an account with Quick Street</h1>
-      <h6>Already have an account. Log In</h6>
-      <form>
-        <label htmlFor='email'>Email</label>
-        <input
-          type='text'
-          name='email'
-          id='email'
-          placeholder='Enter your email'
-          value={values.email}
-          onChange={handleChange}
-        />
-        <label htmlFor='password'>Password</label>
-        <input 
-          type='password'
-          name='password'
-          id='password'
-          placeholder='Please enter a password'
-          value={values.password}
-          onChange={handleChange}
-        />
-        Are you a customer or vendor?
-        <label htmlFor='customer'>Customer</label>
-        <input 
-          type='radio' 
-          name='role' 
-          id='customer' 
-          value='customer'
-          // checked={values.role === 'customer'}
-          onChange={handleChange}
-        />
-        <label htmlFor='vendor'>Vendor</label>
-        <input 
-          type='radio' 
-          name='role' 
-          id='vendor'
-          value='vendor'
-          // checked={values.role === 'vendor'}
-          onChange={handleChange}
-        />
-        <button onClick={proceed}>Next</button>
-      </form>
+    <div className='formContainer'>
+      <div className='signupImage'>
+        {/* <img src={meditateGirlImg} alt='Sign Up Form' /> */}
+      </div>
+      <div className='formDetails'>
+      <h1>Create an account with Quick Street</h1>
+        <h6>Already have an account. Log In</h6>
+        <form>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='text'
+              name='email'
+              id='email'
+              placeholder='Enter your email'
+              value={values.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input 
+              type='password'
+              name='password'
+              id='password'
+              placeholder='Please enter a password'
+              value={values.password}
+              onChange={handleChange}
+            />
+          </div>
+          <h3>Are you a customer or vendor?</h3>
+          <div>
+            <label htmlFor='customer'>Customer</label>
+            <input 
+              type='radio' 
+              name='role' 
+              id='customer' 
+              value='customer'
+              // checked={values.role === 'customer'}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='vendor'>Vendor</label>
+            <input 
+              type='radio' 
+              name='role' 
+              id='vendor'
+              value='vendor'
+              // checked={values.role === 'vendor'}
+              onChange={handleChange}
+            />
+          </div>  
+          <button onClick={proceed}>Next</button>
+        </form>
+      </div>
     </div>
   )
 }
--- app/src/components/VendorDetails.js
+++ app/src/components/VendorDetails.js
@@ -15,56 +15,71 @@ const VendorDetails = (props) => {
 
   return (
     <div>
-      <h1>Great! Let's get you set up.</h1>
-      <form>
-        <label htmlFor='businessName'>Business Name</label>
-        <input
-          type='text'
-          name='businessName'
-          id='businessName'
-          placeholder='Enter your business name'
-          value={values.businessName}
-          onChange={handleChange}
-        />
-        <label htmlFor='phoneNumber'>Phone Number</label>
-        <input
-          type='text'
-          name='phoneNumber'
-          id='phoneNumber'
-          placeholder='Enter your phone number'
-          value={values.phoneNumber}
-          onChange={handleChange}
-        />
-        <label htmlFor='streetAddress'>Street Address</label>
-        <input
-          type='text'
-          name='streetAddress'
-          id='streetAddress'
-          placeholder='Enter your street address'
-          value={values.streetAddress}
-          onChange={handleChange}
-        />
-        <label htmlFor='city'>City</label>
-        <input
-          type='text'
-          name='city'
-          id='city'
-          placeholder='Enter your city'
-          value={values.city}
-          onChange={handleChange}
-        />
-        <label htmlFor='zipcode'>Zipcode</label>
-        <input
-          type='text'
-          name='zipcode'
-          id='zipcode'
-          placeholder='Enter your zipcode'
-          value={values.zipcode}
-          onChange={handleChange}
-        />
-        <button onClick={cancel}>Cancel</button>
-        <button onClick={proceed}>Next</button>
-      </form>
+      <div>
+        <img alt='Signup Image'/>
+      </div>
+      <div>
+        <h1>Great! Let's get you set up.</h1>
+        <form>
+          <div>
+            <label htmlFor='businessName'>Business Name</label>
+            <input
+              type='text'
+              name='businessName'
+              id='businessName'
+              placeholder='Enter your business name'
+              value={values.businessName}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='phoneNumber'>Phone Number</label>
+            <input
+              type='text'
+              name='phoneNumber'
+              id='phoneNumber'
+              placeholder='Enter your phone number'
+              value={values.phoneNumber}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='streetAddress'>Street Address</label>
+            <input
+              type='text'
+              name='streetAddress'
+              id='streetAddress'
+              placeholder='Enter your street address'
+              value={values.streetAddress}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='city'>City</label>
+            <input
+              type='text'
+              name='city'
+              id='city'
+              placeholder='Enter your city'
+              value={values.city}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='zipcode'>Zipcode</label>
+            <input
+              type='text'
+              name='zipcode'
+              id='zipcode'
+              placeholder='Enter your zipcode'
+              value={values.zipcode}
+              onChange={handleChange}
+          />
+          </div>
+          <button onClick={cancel}>Cancel</button>
+          <button onClick={proceed}>Next</button>
+        </form>
+      </div>
     </div>
   )
 }

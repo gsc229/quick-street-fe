91a59b472ce35a56234b5dc2bf29c70b42dc16a4
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-12-27 23:17:19 -0500

    set up redux

---
+++ app/reducers/vendorBulletinReducer.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const GET_POSTS_START = "GET_POSTS_START";
+export const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS";
+export const GET_POSTS_FAIL = "GET_POSTS_FAIL";
+
+export const getPosts = () => dispatch => {};

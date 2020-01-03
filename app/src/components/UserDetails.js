69c45694d9e00a86642a91cffc8e6c30ffbfcb63
Rashmi <poddar.rashmi@gmail.com>
2020-01-02 20:58:14 -0500

    Modified styling

--- app/src/components/UserDetails.js
+++ app/src/components/UserDetails.js
@@ -13,65 +13,67 @@ const UserDetails = (props) => {
   const { values, handleChange} = props;
 
   return (
-    <div className='form_container'>
-      {/* <div className='signup_image'> */}
-      {/* <img src={meditateGirlImg} alt='Sign Up Form' /> */}
-      {/* </div> */}
-      <div className='form_details'>
-      <h1>Create An <br/> Account With <br/> Quick Street</h1>
-        <h3>Already have an account? <Link className='link' to="/login">Log In</Link></h3>
-        <form>
-          <div className='form_input'>
-            <label htmlFor='email'>Email</label>
-            <input
-              type='text'
-              name='email'
-              id='email'
-              // placeholder='Enter your email'
-              value={values.email}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='form_input'>
-            <label htmlFor='password'>Password</label>
-            <input 
-              type='password'
-              name='password'
-              id='password'
-              // placeholder='Please enter a password'
-              value={values.password}
-              onChange={handleChange}
-            />
-          </div>
-          <h4>Are you a vendor?</h4>
-          <div className='form_input_radio'>
-            <div className='radio_buttons'>
-              <input 
-                className='checkbox'
-                type='radio' 
-                name='role' 
-                id='customer' 
-                value='customer'
-                // checked={values.role === 'customer'}
-                onChange={handleChange}
-              />
-              <label htmlFor='customer'>Customer</label>
-            </div>
-            <div className='radio_buttons'>
+    <div>
+      <div className='form_container'>
+        {/* <div className='signup_image'> */}
+        {/* <img src={meditateGirlImg} alt='Sign Up Form' /> */}
+        {/* </div> */}
+        <div className='form_details'>
+        <h1>Create An <br/> Account With <br/> Quick Street</h1>
+          <h3>Already have an account? <Link className='link' to="/login">Log In</Link></h3>
+          <form>
+            <div className='form_input'>
+              <label htmlFor='email'>Email</label>
               <input
-                className='checkbox'
-                type='radio' 
-                name='role' 
-                id='vendor'
-                value='vendor'
-                // checked={values.role === 'vendor'}
+                type='text'
+                name='email'
+                id='email'
+                // placeholder='Enter your email'
+                value={values.email}
                 onChange={handleChange}
               />
-              <label htmlFor='vendor'>Vendor</label>
-            </div>  
-          </div>
-          <button className='confirm_button' onClick={proceed}>Next</button>
-        </form>
+            </div>
+            <div className='form_input'>
+              <label htmlFor='password'>Password</label>
+              <input 
+                type='password'
+                name='password'
+                id='password'
+                // placeholder='Please enter a password'
+                value={values.password}
+                onChange={handleChange}
+              />
+            </div>
+            <h4>Are you a vendor?</h4>
+            <div className='form_input_radio'>
+            <div className='radio_buttons'>
+                <input
+                  className='checkbox'
+                  type='radio' 
+                  name='role' 
+                  id='vendor'
+                  value='vendor'
+                  // checked={values.role === 'vendor'}
+                  onChange={handleChange}
+                />
+                <label htmlFor='vendor'>Yes</label>
+              </div>  
+              <div className='radio_buttons'>
+                <input 
+                  className='checkbox'
+                  type='radio' 
+                  name='role' 
+                  id='customer' 
+                  value='customer'
+                  // checked={values.role === 'customer'}
+                  onChange={handleChange}
+                />
+                <label htmlFor='customer'>No</label>
+              </div>
+            </div>
+            <button className='confirm_button' onClick={proceed}>Next</button>
+          </form>
+        </div>
       </div>
     </div>
   )

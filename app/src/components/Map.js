b39ff0f7f737abb1734a30687d529b5d6914f50c
Rashmi <poddar.rashmi@gmail.com>
2020-01-08 16:04:09 -0500

    Added conditional to display circle on map

--- app/src/components/Map.js
+++ app/src/components/Map.js
@@ -3,12 +3,15 @@ import axios from 'axios';
 
 const Map = (props) => {
 
+  const [ showCircle, setShowCircle ] = useState(false);
+
   const [ mapDetails, setMapDetails ] = useState({
-    lng: 0,
-    lat: 0
+    lng: 48.1454292,
+    lat: -86.7409781
   });
 
   const getGeocode = () => {
+    console.log(props.zipcode);
     axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${props.zipcode}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`)
       .then(response => {
         // console.log(response);
@@ -18,6 +21,7 @@ const Map = (props) => {
           lat: response.data.results[0].geometry.location.lat,
           lng: response.data.results[0].geometry.location.lng,
         })
+        setShowCircle(true);
       })
   };
 
@@ -26,24 +30,28 @@ const Map = (props) => {
   }, [props.zipcode]);
 
   useEffect(() => {
+    console.log('the lat and lng is ', mapDetails.lat, mapDetails.lng);
     let options = {
       center: { lat: mapDetails.lat, lng: mapDetails.lng },
-      zoom: 11, 
-      // zoomControl: false, 
-      // gestureHandling: 'none'
+      zoom: showCircle ? 11 : 8, 
+      zoomControl: false, 
+      gestureHandling: 'none'
     }
     const map = new window.google.maps.Map(document.getElementById('map'), options);
     
-    let cityCircle = new window.google.maps.Circle({
-      strokeColor: '#B706F5',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: 'transparent',
-      fillOpacity: 0.35,
-      map: map,
-      center: { lat: mapDetails.lat, lng: mapDetails.lng },
-      radius: 5000
-    });
+    if (showCircle) { 
+      let cityCircle = new window.google.maps.Circle({
+        strokeColor: '#B706F5',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: 'transparent',
+        fillOpacity: 0.35,
+        map: map,
+        center: { lat: mapDetails.lat, lng: mapDetails.lng },
+        radius: 5000
+      });
+    }
+    
   }, [mapDetails]); 
     
   return (

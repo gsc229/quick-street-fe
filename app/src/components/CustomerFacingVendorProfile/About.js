d69d35a6d0b9475f4b3799b62959f54b6ca3f441
dominique maack <dominiquemaack@gmail.com>
2020-01-13 11:22:48 -0700

    added styling to about us section on customer facing vendor profile

--- app/src/components/CustomerFacingVendorProfile/About.js
+++ app/src/components/CustomerFacingVendorProfile/About.js
@@ -49,12 +49,12 @@ const About = (props) => {
           <p className='title'>Hours of Operation</p>
           <p className='title_content'>{vendor.days_of_week} - {vendor.hours}</p>
           <p className='title'>Contact</p>
-          <p className='title_content'>{vendor.phone}</p>
+          <p className='title_content_phone'>{vendor.phone}</p>
           <p className='title_content'>{vendor.email}</p>
         </div>
         <div className='location_section'>
-          <p>Location</p>
-          <p>The vendor can be found at {vendor.location.zipcode} area</p>
+          <p className='title'>Location</p>
+          <p className='title_content'>The vendor can be found at {vendor.location.zipcode} area</p>
           <Map zipcode={vendor.location.zipcode} />
         </div>
       </div>

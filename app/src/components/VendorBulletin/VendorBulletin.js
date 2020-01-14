66a0f84c08bb7fc1f704daaa62bc0e1ee6fae01e
dominique maack <dominiquemaack@gmail.com>
2020-01-13 21:29:09 -0700

    changed bulletin background and title css

--- app/src/components/VendorBulletin/VendorBulletin.js
+++ app/src/components/VendorBulletin/VendorBulletin.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import VendorAddPostForm from "./VendorAddPostForm";
 import VendorPostList from "./VendorPostList";
 import axios from "axios";
+import bulletin from '../../styles/css/vendor_bulletin.module.css';
 
 const VendorBulletin = props => {
   const [showAddPostForm, setShowAddPostForm] = useState(false);
@@ -46,14 +47,16 @@ const VendorBulletin = props => {
   }, []);
 
   return (
-    <div className="vendor_bulletin_container">
-      <div className="vendor_bulletin_title">
-        <p>Bulletin Board</p>
-        <hr />
-      </div>
+    <div className={bulletin.vendor_bulletin_container}>
+      <div className={bulletin.vendor_bulletin_header}>
+        <div className={bulletin.vendor_bulletin_title}>
+          <p>Bulletin Board</p>
+          {/* <hr /> */}
+        </div>
 
-      <div className="vendor_add_post_btn_wrapper">
-        <button onClick={addPost}>Add Post</button>
+        <div className={bulletin.vendor_add_post_btn_wrapper}>
+          <button className={bulletin.vendor_bulletin_button} onClick={addPost}>Add Post</button>
+        </div>
       </div>
 
       <VendorAddPostForm

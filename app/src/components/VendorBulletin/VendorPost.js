5d1d7b7d4a19fb8a192daecc3d9d389d3f674686
dominique maack <dominiquemaack@gmail.com>
2020-01-13 22:19:29 -0700

    added more styling to vendor bulletin that matches more to customer facing version

--- app/src/components/VendorBulletin/VendorPost.js
+++ app/src/components/VendorBulletin/VendorPost.js
@@ -4,11 +4,13 @@ import post from '../../styles/scss/vendor_post.module.scss';
 
 const VendorPost = ({ content, date }) => {
   return (
-    <li>
-      <p className={post.post_date}>
-        Date {date}
-      </p>
-
+    <li className={post.post_container}>
+      <div className='post_date_line'>
+        <p className={post.post_date}>
+          Date {date}
+        </p>
+        <hr></hr>
+      </div>
       <p className={post.post_content}>{content}</p>
     </li>
   );

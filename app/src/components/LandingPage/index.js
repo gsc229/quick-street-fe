fb77b52c03b4fccc97b7ffb6aecabd851cf0e8ad
Richard Lawson <lawson.a.richard@gmail.com>
2020-01-14 08:14:22 -0800

    reworked footer to be reusable component
    reworked footer to be resuable component and adjusted some styling and text

--- app/src/components/LandingPage/index.js
+++ app/src/components/LandingPage/index.js
@@ -17,7 +17,7 @@ class LandingPage extends React.Component {
 		return (
 			<div className="landingContainer">
 				<div className="heroContainer">
-			<Menu/>
+					<Menu />
 					<div className="landingInfo">
 						<p>Finally , A Way For Vendors and Lovers of Food to Come Together in Harmony </p>
 						<div className="button-container">
@@ -29,112 +29,89 @@ class LandingPage extends React.Component {
 				<div className="marketing-Header"> See What We Offer!</div>
 				<div className="marketing-Container">
 					<div className="marketing-box-top-row">
-					<h1>Browse, buy, share your finds on local food vendors.</h1>
-					<div className="marketing-box-top-row-btn ">Start Browsing</div>
+						<h1>Browse, buy, share your finds on local food vendors.</h1>
+						<div className="marketing-box-top-row-btn ">
+							<Link to="/browse">Start Browsing</Link>
+						</div>
 					</div>
-					<div className="marketing-box-bottom-row"> 
+					<div className="marketing-box-bottom-row">
 						<CarouselTop />
 					</div>
-				
 				</div>
 				<div className="marketing-Header"> How it Works</div>
 				<div className="works-container">
 					<div className="works-card-wrapper">
 						<div className="works-image">
-						<Image name="Works1" />
+							<Image name="Works1" />
 						</div>
 						<div className="works-content">
-							We help vendors gain visibility more consistently to their customer base
+							<p>We help vendors gain visibility more consistently to their customer base.</p>
 						</div>
 					</div>
 					<div className="works-card-wrapper">
 						<div className="works-image">
-						<Image name="Works2" />
+							<Image name="Works2" />
 						</div>
 						<div className="works-content">
-							As the customer, we can seamlessly support your local food vendor through a one stop shop
-							search platform.
+							<p>
+								As the customer, we can seamlessly support your local food vendor through a one stop
+								shop search platform.
+							</p>
 						</div>
 					</div>
 					<div className="works-card-wrapper">
 						<div className="works-image">
-						<Image name="Works3" />
+							<Image name="Works3" />
 						</div>
 						<div className="works-content">
-							See what you like? We have a seamless order request process for both sides. Win-win.
+							<p>See what you like? We have a seamless order request process for both sides. Win-win</p>
 						</div>
 					</div>
 				</div>
 				<div className="marketing-Header"> Our Spotlight Vendors For This Month</div>
 				<div className="carousel-container">
-				<CarouselBrowse />
-
+					<CarouselBrowse />
 				</div>
 				<div className="marketing-Header">Testimonials</div>
 				<div className="works-container">
 					<div className="works-card-wrapper">
 						<div className="works-image">
-						<Image name="Testimonial1" />
+							<Image name="Testimonial1" />
 						</div>
 						<div className="works-content">
-						“I just moved to Austin and wanted to support local from the start. This made it so easy!” -
-							Alandra S.
-							<div className="testimonial-button">
-							Start Now!
-						</div>
-						</div>
-
-					</div>
-					<div className="works-card-wrapper">
-						<div className="works-image">
-						<Image name="Testimonial2" />
-						</div>
-						<div className="works-content">
-						<p>“Quick Street helped me find new found favorite vendors. Its like a whole new city to me!” -
-							Sam B.</p>
-							<div className="testimonial-button">
-							Start Now!
-						</div>
+							<p>
+								“I just moved to Austin and wanted to support local from the start. This made it so
+								easy!” - Alandra S.
+							</p>
+							{/* <div className="testimonial-button">Start Now!</div> */}
 						</div>
 					</div>
 					<div className="works-card-wrapper">
 						<div className="works-image">
-						<Image name="Testimonial3" />
+							<Image name="Testimonial2" />
 						</div>
 						<div className="works-content">
-						<p>“Quick Street helped me find new found favorite vendors. Its like a whole new city to me!” -
-							Sam B.</p>
-							<div className="testimonial-button">
-							Start Now!
+							<p>
+								“Quick Street helped me find new found favorite vendors. Its like a whole new city to
+								me!” - Sam B.
+							</p>
+							{/* <div className="testimonial-button">Start Now!</div> */}
 						</div>
+					</div>
+					<div className="works-card-wrapper">
+						<div className="works-image">
+							<Image name="Testimonial3" />
+						</div>
+						<div className="works-content">
+							<p>
+								“As a vendor this helped so many stressors I used to have throughout my week; now I can
+								focus more time on my craft. It’s great!” - Tracee W.
+							</p>
+							{/* <div className="testimonial-button">Start Now!</div> */}
 						</div>
 					</div>
 				</div>
-
-				<div className="footer-container">
-					<div className="footer-top-row">
-						<div className="footer-top-row-column">
-							<p>Company</p>
-							<p>About Us</p>
-							<p>Careers</p>
-							<p>Contact</p>
-						</div>
-						<div className="footer-top-row-column">
-							<p>Community</p>
-							<p>Blogs</p>
-							<p>Expert Directory</p>
-							<p>Events</p>
-							<p>Food/Vendor Glossary</p>
-						</div>
-						<div className="footer-top-row-column">
-							<p>Product</p>
-							<p>Mobile App</p>
-							<p>Help</p>
-							<p>Swag</p>
-						</div>
-					</div>
 				<Footer />
-				</div>
 			</div>
 		);
 	}

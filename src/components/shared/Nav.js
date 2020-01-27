17a7a1e614a23e74c1bc21c88d543a93856d1968
Luis Ocasio <ocasio.dev@gmail.com>
2020-01-27 14:37:03 -0500

    implemented nav component and initialized styles

--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-//styling
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import navBar from '../../styles/scss/navBar.module.scss';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
 const Nav = () => {
+  const classes = useStyles();
   return (
-    <div className={navBar.container}>
-      <nav class={navBar.nav}>
-        <h1>Market Avenue</h1>
-        <ul class={navBar.links}>
-          <li class={navBar.item}>
-            <Link to='/login'>Login</Link>
-          </li>
-          <li class={navBar.item}>
-            <Link to='/register'>Join Us</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className={classes.root}>
+      <AppBar className={navBar.style} position='static'>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            <h2>Marketavenue</h2>
+          </Typography>
+          <Button color='inherit'>Log In</Button>
+          <Button color='inherit'>Join Us</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };

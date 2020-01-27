c7134321e40969ce7d306fdcfd644394e5207e62
Luis Ocasio <ocasio.dev@gmail.com>
2020-01-27 18:14:11 -0500

    set transparency on nav

--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
@@ -34,7 +33,10 @@ const Nav = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <AppBar position='static'>
+      <AppBar
+        position='static'
+        style={{ background: 'transparent', boxShadow: 'none' }}
+      >
         <Toolbar>
           <IconButton
             edge='start'

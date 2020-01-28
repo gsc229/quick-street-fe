6cb4c33eaa20db595256f0f8f18335400416dd8f
Richard Lawson <lawson.a.richard@gmail.com>
2020-01-28 10:52:57 -0800

    modal styling
    modal styling for BROWSE set

--- src/components/Browse/VendorPage/components/ModalCarousel2.js
+++ src/components/Browse/VendorPage/components/ModalCarousel2.js
@@ -1,26 +1,22 @@
-import React from 'react'
+import React from 'react';
 import { Gallery, GalleryImage } from 'react-gesture-gallery';
-const ModalCarousel2 = ({images}) => {
+const ModalCarousel2 = ({ images }) => {
+	const [ index, setIndex ] = React.useState(0);
 
-  const [index, setIndex] = React.useState(0);
-
-
-  return (
-    <div>
-      <div style={{ background: 'white', width: '100%', height: '100%' }}>
-        <Gallery
-          index={index}
-          onRequestChange={(i) => {
-            setIndex(i);
-          }}
-        >
-          {images.map((img) => <GalleryImage objectFit="contain" key={img._id} src={img.secure_url} />)}
-        </Gallery>
-      </div>
+	return (
+		<div>
+			<div style={{ background: 'white', width: '100%', height: '100%' }}>
+				<Gallery
+					index={index}
+					onRequestChange={(i) => {
+						setIndex(i);
+					}}
+				>
+					{images.map((img) => <GalleryImage objectFit="contain" key={img._id} src={img.secure_url} />)}
+				</Gallery>
+			</div>
+		</div>
 	);
-    
-    </div>
-  )
-}
+};
 
-export default ModalCarousel2
+export default ModalCarousel2;

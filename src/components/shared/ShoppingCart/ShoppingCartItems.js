3f0dba6966b7ca8fc5a11a710186f6ee212806d3
Rashmi <poddar.rashmi@gmail.com>
2020-01-31 11:42:44 -0500

    Added a console.log statement to view state

--- src/components/shared/ShoppingCart/ShoppingCartItems.js
+++ src/components/shared/ShoppingCart/ShoppingCartItems.js
@@ -5,6 +5,7 @@ import { ShoppingCartItem } from '../../index';
 
 const ShoppingCartItems = ({ setCartModal }) => {
   const { state, getCartItems } = useContext(CartContext);
+  console.log('state in shopping cart items', state);
   const cart = state.cart;
   const customerId = localStorage.getItem('user_id');
 

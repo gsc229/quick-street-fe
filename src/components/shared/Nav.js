bcd4f50843a158e7587b8c2c85b4d79c62d06bc1
Luis Ocasio <ocasio.dev@gmail.com>
2020-02-03 15:23:51 -0500

    setup landing nav

--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -22,244 +22,279 @@ import Modal from './Modal';
 import ShoppingCartItems from './ShoppingCart/ShoppingCartItems';
 
 import { shopping_cart_light } from '../../assets/svgs/index';
-const useStyles = makeStyles((theme) => ({
-	avatar: {
-		height: '42px',
-		width: '42px'
-	},
-	numbder: {
-		position: 'absolute'
-	},
-	grow: {
-		flexGrow: 1
-	},
-	menuButton: {
-		marginRight: theme.spacing(2)
-	},
-	title: {
-		display: 'none',
-		[theme.breakpoints.up('lg')]: {
-			display: 'block'
-		}
-	},
-	search: {
-		position: 'relative',
-		borderRadius: theme.shape.borderRadius,
-		backgroundColor: fade(theme.palette.common.white, 0.15),
-		'&:hover': {
-			backgroundColor: fade(theme.palette.common.white, 0.25)
-		},
-		marginRight: theme.spacing(2),
-		marginLeft: 0,
-		width: '100%',
-		[theme.breakpoints.up('sm')]: {
-			marginLeft: theme.spacing(3),
-			width: 'auto'
-		}
-	},
-	searchIcon: {
-		width: theme.spacing(7),
-		height: '100%',
-		position: 'absolute',
-		pointerEvents: 'none',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	inputRoot: {
-		color: 'inherit'
-	},
-	inputInput: {
-		padding: theme.spacing(1, 1, 1, 7),
-		transition: theme.transitions.create('width'),
-		width: '100%',
-		[theme.breakpoints.up('md')]: {
-			width: 200
-		}
-	},
-	sectionDesktop: {
-		display: 'none',
-		[theme.breakpoints.up('md')]: {
-			display: 'flex'
-		}
-	},
-	sectionMobile: {
-		display: 'flex',
-		[theme.breakpoints.up('md')]: {
-			display: 'none'
-		}
-	}
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    height: '42px',
+    width: '42px'
+  },
+  numbder: {
+    position: 'absolute'
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    color: 'white',
+    display: 'none',
+    [theme.breakpoints.up('lg')]: {
+      display: 'block'
+    }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200
+    }
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
+    },
+    links: {
+      color: 'white'
+    }
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
+  }
 }));
 const Nav = () => {
-	const { signout } = useContext(AuthContext);
-	const { state, getCartItems } = useContext(CartContext);
-	const classes = useStyles();
-	const [ anchorEl, setAnchorEl ] = React.useState(null);
-	const [ mobileMoreAnchorEl, setMobileMoreAnchorEl ] = React.useState(null);
+  const { signout } = useContext(AuthContext);
+  const { state, getCartItems } = useContext(CartContext);
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-	const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
 
-	const [ cartModal, setCartModal ] = useState(false);
+  const [cartModal, setCartModal] = useState(false);
 
-	console.log('our token', { token });
+  console.log('our token', { token });
 
-	const isMenuOpen = Boolean(anchorEl);
-	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-	const handleProfileMenuOpen = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleProfileMenuOpen = event => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleMobileMenuClose = () => {
-		setMobileMoreAnchorEl(null);
-	};
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
 
-	const handleMenuClose = () => {
-		setAnchorEl(null);
-		handleMobileMenuClose();
-	};
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
 
-	const handleMobileMenuOpen = (event) => {
-		setMobileMoreAnchorEl(event.currentTarget);
-	};
+  const handleMobileMenuOpen = event => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
 
-	const menuId = 'primary-search-account-menu';
-	const renderMenu = (
-		<Menu
-			anchorEl={anchorEl}
-			anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-			id={menuId}
-			keepMounted
-			transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-			open={isMenuOpen}
-			onClose={handleMenuClose}
-		>
-			<MenuItem onClick={handleMenuClose}>Dashboard</MenuItem>
-			<MenuItem onClick={handleMenuClose}>
-				<Link to="/login" onClick={() => signout()}>
-					Sign Out
-				</Link>
-			</MenuItem>
-		</Menu>
-	);
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Dashboard</MenuItem>
+      <MenuItem onClick={handleMenuClose}>
+        <Link to='/login' onClick={() => signout()}>
+          Sign Out
+        </Link>
+      </MenuItem>
+    </Menu>
+  );
 
-	const mobileMenuId = 'primary-search-account-menu-mobile';
-	const renderMobileMenu = (
-		<Menu
-			anchorEl={mobileMoreAnchorEl}
-			anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-			id={mobileMenuId}
-			keepMounted
-			transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-			open={isMobileMenuOpen}
-			onClose={handleMobileMenuClose}
-		>
-			<MenuItem>
-				<IconButton aria-label="show 4 new mails" color="inherit">
-					<Badge badgeContent={4} color="secondary">
-						<MailIcon />
-					</Badge>
-				</IconButton>
-				<p>Messages</p>
-			</MenuItem>
-			<MenuItem>
-				<IconButton aria-label="show 11 new notifications" color="inherit">
-					<Badge badgeContent={11} color="secondary">
-						<NotificationsIcon />
-					</Badge>
-				</IconButton>
-				<p>Notifications</p>
-			</MenuItem>
-			<MenuItem onClick={handleProfileMenuOpen}>
-				<IconButton
-					aria-label="account of current user"
-					aria-controls="primary-search-account-menu"
-					aria-haspopup="true"
-					color="inherit"
-				>
-					<AccountCircle />
-				</IconButton>
-				<p>Profile</p>
-			</MenuItem>
-		</Menu>
-	);
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label='show 4 new mails' color='inherit'>
+          <Badge badgeContent={4} color='secondary'>
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label='show 11 new notifications' color='inherit'>
+          <Badge badgeContent={11} color='secondary'>
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label='account of current user'
+          aria-controls='primary-search-account-menu'
+          aria-haspopup='true'
+          color='inherit'
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
-	return (
-		<div className={classes.grow}>
-			<AppBar style={{ background: '#00b2ed', boxShadow: 'none' }} position="static">
-				<Toolbar>
-					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="open drawer">
-						<MenuIcon />
-					</IconButton>
-					<Typography className={classes.title} variant="h6" noWrap>
-						Market Avenue
-					</Typography>
+  return (
+    <div className={classes.grow}>
+      <AppBar
+        style={{ background: 'transparent', boxShadow: 'none' }}
+        position='static'
+      >
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='open drawer'
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant='h6' noWrap>
+            Market Avenue
+          </Typography>
 
-					<div className={classes.grow} />
+          <div className={classes.grow} />
 
-					<div className={classes.sectionDesktop}>
-						{!token && (
-							<div className={classes.sectionDesktop}>
-								<Link to="/login">login</Link>
-								<Link to="/register">Join us</Link>
-							</div>
-						)}
-					</div>
-					<div className={classes.sectionDesktop}>
-						{/* <IconButton aria-label='show 17 new notifications' color='inherit'>
+          <div className={classes.sectionDesktop}>
+            {!token && (
+              <div className={classes.sectionDesktop}>
+                <Link
+                  style={{
+                    margin: '1rem',
+                    color: 'white',
+                    textDecoration: 'none',
+                    fontSize: '2.4rem'
+                  }}
+                  to='/login'
+                >
+                  Login
+                </Link>
+                <Link
+                  style={{
+                    margin: '1rem',
+                    color: 'white',
+                    textDecoration: 'none',
+                    fontSize: '2.4rem'
+                  }}
+                  to='/register'
+                >
+                  Join Us
+                </Link>
+              </div>
+            )}
+          </div>
+          <div className={classes.sectionDesktop}>
+            {/* <IconButton aria-label='show 17 new notifications' color='inherit'>
               <Badge badgeContent={17} color='secondary'>
                 <NotificationsIcon />
               </Badge>
             </IconButton> */}
-						{token && (
-							<IconButton
-								edge="end"
-								aria-label="account of current user"
-								aria-controls={menuId}
-								aria-haspopup="true"
-								onClick={handleProfileMenuOpen}
-								color="inherit"
-							>
-								<AccountCircle style={{ height: '42px', width: '42px' }} />
-							</IconButton>
-						)}
-					</div>
-					<MenuItem>
-						{token && (
-							<div>
-								<p onClick={() => setCartModal(true)}>
-									<img
-										src={shopping_cart_light}
-										style={{ height: '3rem', width: '3rem' }}
-										alt="shopping cart"
-									/>
-									{state.cart.items && (
-										<span className={classes.number}> {state.cart.items.length}</span>
-									)}
-								</p>
-								<Modal showModal={cartModal}>
-									<ShoppingCartItems setCartModal={setCartModal} />
-								</Modal>
-							</div>
-						)}
-					</MenuItem>
-					<div className={classes.sectionMobile}>
-						<IconButton
-							aria-label="show more"
-							aria-controls={mobileMenuId}
-							aria-haspopup="true"
-							onClick={handleMobileMenuOpen}
-							color="inherit"
-						>
-							<MoreIcon />
-						</IconButton>
-					</div>
-				</Toolbar>
-			</AppBar>
-			{renderMobileMenu}
-			{renderMenu}
-		</div>
-	);
+            {token && (
+              <IconButton
+                edge='end'
+                aria-label='account of current user'
+                aria-controls={menuId}
+                aria-haspopup='true'
+                onClick={handleProfileMenuOpen}
+                color='inherit'
+              >
+                <AccountCircle style={{ height: '42px', width: '42px' }} />
+              </IconButton>
+            )}
+          </div>
+          <MenuItem>
+            {token && (
+              <div>
+                <p onClick={() => setCartModal(true)}>
+                  <img
+                    src={shopping_cart_light}
+                    style={{ height: '3rem', width: '3rem' }}
+                    alt='shopping cart'
+                  />
+                  {state.cart.items && (
+                    <span className={classes.number}>
+                      {' '}
+                      {state.cart.items.length}
+                    </span>
+                  )}
+                </p>
+                <Modal showModal={cartModal}>
+                  <ShoppingCartItems setCartModal={setCartModal} />
+                </Modal>
+              </div>
+            )}
+          </MenuItem>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label='show more'
+              aria-controls={mobileMenuId}
+              aria-haspopup='true'
+              onClick={handleMobileMenuOpen}
+              color='inherit'
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
 };
 
 export default Nav;

0fe74a411307c43835130296b3604fd24ab68c5f
dominique maack <dominiquemaack@gmail.com>
2020-02-04 11:21:41 -0700

    clearing cart and adding a new one upon successful payment in stripe checkout button component

--- src/pages/StripeCheckoutButton.js
+++ src/pages/StripeCheckoutButton.js
@@ -1,10 +1,16 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import axiosWithAuth from '../utils/axiosWithAuth';
-import {Context as CartContext} from '../../../contexts/TestCartContext';
+import {Context as CartContext} from '../contexts/TestCartContext';
 
 const StripeCheckoutButton = ({ price, customerId }) => {
-  const { state, getCartItems } = useContext(CartContext);
+  const { state, deleteAndAddCart } = useContext(CartContext);
+  
+  let cart = state.cart;
+  console.log('do i get a cart id',cart)
+
+
+  
   
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_h1PiAqFdpVJpFn9xYKA1JEX7008fXbJlqI';
@@ -12,16 +18,20 @@ const StripeCheckoutButton = ({ price, customerId }) => {
     const onToken = token => {
       console.log(token);
       //alert('Payment Succesful!');
+
+      cart = cart._id
       axiosWithAuth()
         .post(`/customers/${customerId}/cart/payment`, {
             totalPrice: priceForStripe,
             token: token,
             currency: 'usd'
         }).then(res => {
-            console.log(res)
+            console.log('response from successful token',res)
             // clear cart state
-           
+         
+          deleteAndAddCart({customerId, cartId: cart});
             // show confirmation 
+
         }).catch(err => {
             console.log(err)
         })

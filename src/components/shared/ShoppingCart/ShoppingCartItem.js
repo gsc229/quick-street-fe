967810d0ba5a069adf7738591c279cc8cd85dfb9
Richard Lawson <lawson.a.richard@gmail.com>
2020-02-04 19:52:49 -0800

    need to finish styling for size
    need to finish styling for size

--- src/components/shared/ShoppingCart/ShoppingCartItem.js
+++ src/components/shared/ShoppingCart/ShoppingCartItem.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Image, CloudinaryContext, Transformation } from 'cloudinary-react';
 
+// styles
+import shopping from '../../../styles/scss/shopping.module.scss';
+
 const ShoppingCartItem = ({ product }) => {
 	return (
 		<React.Fragment>
-			{/* {console.log(product.item.product_image.thumbnail_url)} */}
-			{product.item.product_image && (
-				<CloudinaryContext cloudName="quickstlabs">
-					<Image publicId={product.item.product_image.public_id}>
-						<Transformation gravity="center" height="92" width="110" crop="fill" />
-					</Image>
-				</CloudinaryContext>
-			)}
-			<p>{product.quantity} </p>
-			<p>
-				{product.item.name} per {product.item.unit}
-			</p>
+			<div className={shopping.wrapper}>
+				<div className={shopping.left}>
+					{/* {console.log(product.item.product_image.thumbnail_url)} */}
+					{product.item.product_image && (
+						<CloudinaryContext cloudName="quickstlabs">
+							<Image publicId={product.item.product_image.public_id}>
+								<Transformation gravity="center" height="92" width="110" crop="fill" />
+							</Image>
+						</CloudinaryContext>
+					)}
+				</div>
+				<div className={shopping.right}>
+					<p>
+						{product.quantity} {product.item.name}
+					</p>
+					<p>
+						${product.item.price} per {product.item.unit}
+					</p>
+				</div>
+			</div>
 		</React.Fragment>
 	);
 };

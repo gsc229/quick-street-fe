a8d2f9aabb0630ab257a2027f8b39c177ae959c3
Greg <gsc229@gmail.com>
2020-02-05 11:30:22 -0500

    pull from master

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { About, VendorProducts, Bulletin, BannerUploader, Nav } from '../components/index';
+import {
+  About,
+  VendorProducts,
+  Bulletin,
+  BannerUploader,
+  Nav
+} from '../components/index';
 import { Placeholder } from '../assets/images/index';
 //Styles
 import profile from '../styles/scss/vendor/a_vendors_profile.module.scss';
@@ -12,63 +18,66 @@ import axiosWithAuth from '../utils/axiosWithAuth';
 import Footer from '../components/shared/Footer';
 import browse from '../styles/scss/browse.module.scss';
 
-const Profile = (props) => {
-	// It all starts here!...with vendorId from localStorage
-	const [ vendorId, setVendorId ] = useState(localStorage.getItem('user_id'));
-	const [ vendorInfo, setVendorInfo ] = useState({
-		location: {
-			zipcode: '18641'
-		}
-	});
-	const [ bannerInfo, setBannerInfo ] = useState('no_banner.jpg');
-	const [ products, setProducts ] = useState([]);
-	const [ productImagesIds, setProductImagesIds ] = useState([]);
+const Profile = props => {
+  // It all starts here!...with vendorId from localStorage
+  const [vendorId, setVendorId] = useState(localStorage.getItem('user_id'));
+  const [vendorInfo, setVendorInfo] = useState({
+    location: {
+      zipcode: '18641'
+    }
+  });
+  const [bannerInfo, setBannerInfo] = useState('no_banner.jpg');
+  const [products, setProducts] = useState([]);
+  const [productImagesIds, setProductImagesIds] = useState([]);
 
-	// Booleans
+  // Booleans
 
-	const [ editingName, setEditingName ] = useState(false);
-	// bool for reloading products after product update.
-	const [ reloadProducts, setReloadProducts ] = useState(false);
-	const [ editAbout, setEditAbout ] = useState(false);
-	const [ editBusinessName, setEditBusinessName ] = useState(false);
-	//console.log('Profile.js vendorInfo: ', vendorInfo);
+  const [editingName, setEditingName] = useState(false);
+  // bool for reloading products after product update.
+  const [reloadProducts, setReloadProducts] = useState(false);
+  const [editAbout, setEditAbout] = useState(false);
+  const [editBusinessName, setEditBusinessName] = useState(false);
+  //console.log('Profile.js vendorInfo: ', vendorInfo);
 
-	useEffect(
-		() => {
-			//console.log('USEEFFECT 1 Profile.js');
-			axiosWithAuth()
-				.get(`/vendors/${vendorId}`)
-				.then((response) => {
-					setVendorInfo(response.data.data);
-					/* setBannerInfo(vendorInfo.data.data.vendor_banner); */
-					console.log('GET useEffect Profile.js setVendorInfo(response)', response);
-				})
-				.catch((error) => {
-					console.log('ERROR Profile.js GET vendors/:vendorId error: ', error);
-				});
-		},
-		[ vendorId ]
-	);
+  useEffect(() => {
+    //console.log('USEEFFECT 1 Profile.js');
+    axiosWithAuth()
+      .get(`/vendors/${vendorId}`)
+      .then(response => {
+        setVendorInfo(response.data.data);
+        /* setBannerInfo(vendorInfo.data.data.vendor_banner); */
+        console.log(
+          'GET useEffect Profile.js setVendorInfo(response)',
+          response
+        );
+      })
+      .catch(error => {
+        console.log('ERROR Profile.js GET vendors/:vendorId error: ', error);
+      });
+  }, [vendorId]);
 
-	useEffect(
-		() => {
-			//console.log('USEEFFECT 2 Profile.js');
-			axiosWithAuth()
-				.get(`/vendors/${vendorId}/products`)
-				.then((response) => {
-					console.log('GET Profile.js /vendors/:vendorId/products response', response);
-					setProducts(response.data.data);
-				})
-				.catch((error) => {
-					console.log('ERROR Profile.js GET fetchProducts() vendors/:vendorId/products error: ', error);
-				});
-		},
-		[ vendorId, reloadProducts, setReloadProducts ]
-	);
+  useEffect(() => {
+    //console.log('USEEFFECT 2 Profile.js');
+    axiosWithAuth()
+      .get(`/vendors/${vendorId}/products`)
+      .then(response => {
+        console.log(
+          'GET Profile.js /vendors/:vendorId/products response',
+          response
+        );
+        setProducts(response.data.data);
+      })
+      .catch(error => {
+        console.log(
+          'ERROR Profile.js GET fetchProducts() vendors/:vendorId/products error: ',
+          error
+        );
+      });
+  }, [vendorId, reloadProducts, setReloadProducts]);
 
-	const editName = () => {
-		setEditBusinessName(!editBusinessName);
-	};
+  const editName = () => {
+    setEditBusinessName(!editBusinessName);
+  };
 
   const saveName = () => {
     axiosWithAuth()
@@ -85,29 +94,31 @@ const Profile = (props) => {
       });
   };
 
-	const editProfile = () => {
-		// console.log(`edit profile clicked`);
-		setEditAbout(!editAbout);
-	};
+  const editProfile = () => {
+    // console.log(`edit profile clicked`);
+    setEditAbout(!editAbout);
+  };
 
-	const saveProfile = (e) => {
-		if (e) {
-			e.preventDefault();
-		}
+  const saveProfile = e => {
+    if (e) {
+      e.preventDefault();
+    }
 
-		//console.log('SAVE PROFILE vendorInfo', vendorInfo);
-		axiosWithAuth()
-			.put(`https://quickstlabs.herokuapp.com/api/v1.0/vendors/${vendorId}`, vendorInfo)
-			.then((res) => {
-				console.log(`Profile.js saveProfile()`, res);
-				setVendorInfo(res.data.data);
-			})
-			.catch((err) => {
-				console.log('VendorProf. PUT error ', err);
-			});
-	};
+    //console.log('SAVE PROFILE vendorInfo', vendorInfo);
+    axiosWithAuth()
+      .put(
+        `https://quickstlabs.herokuapp.com/api/v1.0/vendors/${vendorId}`,
+        vendorInfo
+      )
+      .then(res => {
+        console.log(`Profile.js saveProfile()`, res);
+        setVendorInfo(res.data.data);
+      })
+      .catch(err => {
+        console.log('VendorProf. PUT error ', err);
+      });
+  };
 
-<<<<<<< HEAD
   return (
     <React.Fragment>
       <div className={browse.temp_menu}>
@@ -158,95 +169,61 @@ const Profile = (props) => {
                 )}
               </div>
             </div>
-=======
-	return (
-		<React.Fragment>
-			<div style={{ backgroundColor: '#00B2ED' }} className={browse.temp_menu}>
-				<Nav />
-			</div>
-			<div className={profile.profile_container}>
-				<div className={`${profile.wrapper} ${banner.banner_wrapper}`}>
-					<div className={`${profile.inner_container} ${banner.inner_banner_container}`}>
-						<div className={banner.banner_text_btns}>
-							<div className={banner.vendor_header_name}>
-								{editingName ? ( // <<<<<<<<<< turnary ?
-									<React.Fragment>
-										<input
-											className={banner.business_name_input}
-											onChange={(e) => {
-												if (editBusinessName) {
-													setVendorInfo({
-														...vendorInfo,
-														business_name: e.target.value
-													});
-												}
-											}}
-											value={vendorInfo.business_name}
-										/>
-										<div
-											className={banner.edit_guides}
-											onClick={() => {
-												setEditingName(false);
-												console.log('CLICKED');
-											}}
-										>
-											<p className={`${banner.save_changes}`}>
-												<i className="fa fa-save" />&nbsp; save
-											</p>
-										</div>
-									</React.Fragment>
-								) : (
-									//<<<<<<< TURNARY input name or p tag
-									<h1 onClick={() => setEditingName(true)} className={banner.business_name_text}>
-										{vendorInfo.business_name}
-									</h1>
-								)}
-							</div>
-						</div>
->>>>>>> f92f28af146c9517320c0332813dee7676a5c181
 
-						<div className={banner.vendor_banner_image_container}>
-							{vendorInfo.vendor_banner !== `no-photo.jpg` ? (
-								<CloudinaryContext cloudName="quickstlabs">
-									<Image className={banner.vendor_banner_image} publicId={vendorInfo.vendor_banner}>
-										<Transformation gravity="center" height="500" width="1800" crop="fill" />
-									</Image>
-								</CloudinaryContext>
-							) : (
-								<img className="vendor_banner_image" src={Placeholder} alt="vendor header" />
-							)}
+            <div className={banner.vendor_banner_image_container}>
+              {vendorInfo.vendor_banner !== `no-photo.jpg` ? (
+                <CloudinaryContext cloudName='quickstlabs'>
+                  <Image
+                    className={banner.vendor_banner_image}
+                    publicId={vendorInfo.vendor_banner}
+                  >
+                    <Transformation
+                      gravity='center'
+                      height='500'
+                      width='1800'
+                      crop='fill'
+                    />
+                  </Image>
+                </CloudinaryContext>
+              ) : (
+                <img
+                  className='vendor_banner_image'
+                  src={Placeholder}
+                  alt='vendor header'
+                />
+              )}
 
-							<BannerUploader
-								vendorId={vendorId}
-								vendorInfo={vendorInfo}
-								setBannerInfo={setBannerInfo}
-								bannerInfo={bannerInfo}
-							/>
-						</div>
-					</div>
-					{/* Inner Banner Container */}
-				</div>
-				{/* END BANNER WRAPPER */}
+              <BannerUploader
+                vendorId={vendorId}
+                vendorInfo={vendorInfo}
+                setBannerInfo={setBannerInfo}
+                bannerInfo={bannerInfo}
+              />
+            </div>
+          </div>
+          {/* Inner Banner Container */}
+        </div>
+        {/* END BANNER WRAPPER */}
 
-				<About
-					vendorInfo={vendorInfo}
-					editAbout={editAbout}
-					editProfile={editProfile}
-					saveProfile={saveProfile}
-					setVendorInfo={setVendorInfo}
-				/>
-				<VendorProducts
-					setReloadProducts={setReloadProducts}
-					reloadProducts={reloadProducts}
-					reloadProducts={reloadProducts}
-					products={products}
-					vendorId={vendorInfo._id}
-				/>
-				<Bulletin vendorId={vendorId} />
-			</div>
-			<Footer />
-		</React.Fragment>
-	);
+        <About
+          vendorInfo={vendorInfo}
+          editAbout={editAbout}
+          editProfile={editProfile}
+          saveProfile={saveProfile}
+          setVendorInfo={setVendorInfo}
+        />
+        <VendorProducts
+          setReloadProducts={setReloadProducts}
+          reloadProducts={reloadProducts}
+          reloadProducts={reloadProducts}
+          products={products}
+          vendorId={vendorInfo._id}
+        />
+        <Bulletin vendorId={vendorId} />
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default Profile;
